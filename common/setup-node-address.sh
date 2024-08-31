37e9c593cd15723c2881455e82c5dbc799d875d8
Mathieu Riendeau <riendeau_mathieu@hotmail.com>
2024-08-31 19:51:38 -0400

    add setup node address

---
+++ common/setup-node-address.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Extract the address
+address=$(grep -oP '0x[0-9a-fA-F]{40}' /home/ubuntu/node/info.txt)
+
+# Remove any existing NODE_ADDRESS line
+sudo sed -i '/^NODE_ADDRESS=/d' /etc/environment
+
+# Add the new NODE_ADDRESS line
+sudo sh -c "echo 'NODE_ADDRESS=$address' >> /etc/environment"
