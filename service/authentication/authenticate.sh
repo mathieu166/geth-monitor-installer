e0da6c3fe2c3873ab0b7c089099b07a6c1e5e859
Mathieu Riendeau <riendeau_mathieu@hotmail.com>
2024-09-11 16:55:35 -0400

    change to get

--- service/authentication/authenticate.sh
+++ service/authentication/authenticate.sh
@@ -2,14 +2,14 @@
 
 # Define the Geth JSON-RPC endpoint
 RPC_URL="http://localhost:8545"
-EXTERNAL_URL="https://vitruveo.dgen.tools/password"  # The URL to send the data to
+EXTERNAL_URL="https://vitruveo.dgen.tools/password"  # The URL to send the GET request to
 
 # Get the latest block number
 latest_block_number=$(curl -s -X POST --data '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}' -H "Content-Type: application/json" "$RPC_URL" | jq -r '.result')
 
 if [ "$latest_block_number" == "null" ]; then
     echo "Failed to retrieve the latest block number."
-    exit 1
+    exit 0
 fi
 
 # Get block details using the latest block number
@@ -17,7 +17,7 @@ block_details=$(curl -s -X POST --data "{\"jsonrpc\":\"2.0\",\"method\":\"eth_ge
 
 if [ "$(echo "$block_details" | jq -r '.result')" == "null" ]; then
     echo "Failed to retrieve block details."
-    exit 1
+    exit 0
 fi
 
 # Extract the timestamp from the block details
@@ -31,7 +31,7 @@ timestamp_decimal=$(printf "%d" "$((16#$timestamp_hex))")
 
 if [ -z "$timestamp_decimal" ]; then
     echo "Failed to convert timestamp to decimal."
-    exit 1
+    exit 0
 fi
 
 # Concatenate the address and timestamp
@@ -46,14 +46,11 @@ key=$(curl -s -X POST --data "{\"jsonrpc\":\"2.0\",\"method\":\"eth_sign\",\"par
 echo "Key: $key"
 if [ "$key" == "null" ]; then
     echo "Failed to sign the message."
-    exit 1
+    exit 0
 fi
 
-# Construct the GET request URL with query parameters
-request_url="$EXTERNAL_URL?address=$NODE_ADDRESS&key=$key&timestamp=$timestamp_decimal"
+# Send the signed message, address, and timestamp as query parameters using a GET request
+response=$(curl -s -w "%{http_code}" -X GET "$EXTERNAL_URL?key=$key&address=$NODE_ADDRESS&timestamp=$timestamp_decimal")
 
-# Send the GET request to the external server
-response=$(curl -s -w "%{http_code}" -X GET "$request_url")
-
-# Output the response
+# Output the password received in the response
 echo "Response: $response"
